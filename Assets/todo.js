c6c0ab45e5ce6d4871d6f24309a10b143a0c372c
Tarun <87527923+Tarungoar22@users.noreply.github.com>
2024-06-10 20:52:19 +0530

    html Id's implement in js

---
+++ Assets/todo.js
@@ -0,0 +1,7 @@
+const addButton= document.getElementById("addButton");
+const userName= document.getElementById("userName");
+
+addButton.onclick = () =>{
+    const newNameval = userName.value
+    alert(newNameval)
+}
