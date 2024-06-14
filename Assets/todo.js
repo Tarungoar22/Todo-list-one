b906033634e99806d903d54c0f9ef0be66d19cb2
Tarun <87527923+Tarungoar22@users.noreply.github.com>
2024-06-14 15:36:21 +0530

    updated code

---
+++ Assets/todo.js
@@ -0,0 +1,10 @@
+let userName = document.getElementById('userName')
+let textAdd = document.querySelector('text-add');
+
+function AddBtn () {
+   if(userName.value == ""){
+    alert("fill you task")
+   }else{
+
+   }
+}
