4f5290bb3586f6d6e57c9cb5481249b5af71ed94
Tarun <87527923+Tarungoar22@users.noreply.github.com>
2024-06-15 01:53:15 +0530

    add and delete functionality update

--- Assets/todo.js
+++ Assets/todo.js
@@ -1,10 +1,24 @@
 let userName = document.getElementById('userName')
-let textAdd = document.querySelector('text-add');
+let text = document.querySelector('.textadd');
 
 function AddBtn () {
    if(userName.value == ""){
     alert("fill you task")
    }else{
+    let newEle= document.createElement("li");
+    newEle.innerHTML= `${userName.value} <button class="edit btn btn-warning btn-sm ms-2">Edit</button> <button class="remove btn btn-danger btn-sm ms-2">Remove</button>` ;
+    text.appendChild(newEle)
+    userName.value = ""    ;// input text empty
+    let removeBtn = newEle.querySelector(".remove");
+    removeBtn.addEventListener("click", function() {
+        newEle.remove();
+    });
+
+    let editbtn = newEle.querySelector(".edit");
+    editbtn.addEventListener("click", function(){
+        
+    });
+
 
    }
 }
